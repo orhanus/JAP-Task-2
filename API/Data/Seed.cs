907dd75da138558de75f2b257e17ae7b6502db74
orhanus <orhanus@maestralsolutions.com>
2021-09-06 22:50:26 +0200

    Prepared data and database to seed database

---
+++ API/Data/Seed.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedShows(DataContext context)
+        {
+            if(await context.Shows.AnyAsync()) return;
+
+            var showData = await System.IO.File.ReadAllTextAsync("Data/ShowSeedData");
+            var shows = JsonSerializer.Deserialize<List<Show>>(showData);
+
+            foreach(var show in shows)
+            {
+                context.Shows.Add(show);
+            }
+
+            await context.SaveChangesAsync();
+        }
+        
+    }
+}
--- API/Entities/Rating.cs
+++ API/Entities/Rating.cs
@@ -6,7 +6,7 @@ namespace API.Entities
     public class Rating
     {
         public int Id { get; set; }
-        public double Score { get; set; }
+        public int Score { get; set; }
         public Show Show { get; set; }
         public int ShowId { get; set; }
     }
