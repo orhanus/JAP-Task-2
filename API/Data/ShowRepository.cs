5e9ec00f37150f43d13920af138f997ebb5984df
orhanus <orhanus@maestralsolutions.com>
2021-09-13 13:10:06 +0200

    Added keyword processing on API

--- API/Data/ShowRepository.cs
+++ API/Data/ShowRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
@@ -8,6 +9,7 @@ using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using LinqKit;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -31,6 +33,66 @@ namespace API.Data
         {
             return await _context.Actors.ToListAsync();
         }
+        private Dictionary<string, int> GetKeywords(string searchParams)
+        {
+            string[] keywordsArray = {"older than ", "after "};
+            Dictionary<string, int> Keywords = new Dictionary<string, int>();
+            searchParams = searchParams.ToLower();
+
+            int index = searchParams.IndexOf(keywordsArray[1]);
+            if(index != -1)
+            {
+                Keywords.Add("after", Int32.Parse(searchParams.Substring(index + keywordsArray[0].Length, 4)));
+            }
+
+            index = searchParams.IndexOf(keywordsArray[0]);
+            if(index != -1)
+            {
+                Keywords.Add("olderthan", Int32.Parse(searchParams.Substring(index + keywordsArray[0].Length, 1)));
+            }
+
+            bool starFilter = false;
+            try {
+                Regex regex = new Regex("at least ([1-5]) stars");
+                Match match = regex.Match(searchParams);
+                Keywords.Add("atleast", Int32.Parse(match.Groups[1].Value) * 2);
+                starFilter = true;
+            }
+            catch(Exception e){}
+
+            if(!starFilter)
+            {
+                try {
+                Regex regex = new Regex("([1-5]) stars");
+                Match match = regex.Match(searchParams);
+                Keywords.Add("stars", Int32.Parse(match.Groups[1].Value) * 2);
+            }
+            catch(Exception e){}
+            }
+
+            return Keywords;
+        }
+
+        private IQueryable<ShowDto> ApplySearchParameters(IQueryable<ShowDto> query, string searchParams)
+        {
+            Dictionary<string, int> keywords = GetKeywords(searchParams);
+            if(keywords.Count == 0)
+                query = query.Where(obj => obj.Title.ToLower().Contains(searchParams.ToLower()) 
+                    || obj.Description.ToLower().Contains(searchParams.ToLower()));
+            else
+            {
+                if(keywords.ContainsKey("olderthan"))
+                    query = query.Where(show => DateTime.Now.Year - show.ReleaseDate.Year >= keywords["olderthan"]);
+                if(keywords.ContainsKey("after"))
+                    query = query.Where(show => show.ReleaseDate.Year >= keywords["after"]);
+                if(keywords.ContainsKey("atleast"))
+                    query = query.Where(show => show.AverageRating >= keywords["atleast"]);
+                if(keywords.ContainsKey("stars"))
+                    query = query.Where(show => (int) show.AverageRating == keywords["stars"]);
+            }
+
+            return query;
+        }
 
 
         public async Task<PagedList<ShowDto>> GetShowsAsync(ShowParams showParams, string showType)
@@ -51,8 +113,7 @@ namespace API.Data
                 query = query.Where(x => x.ShowType == showType);
 
             if(showParams.SearchParams != "null")
-                query = query.Where(obj => obj.Title.ToLower().Contains(showParams.SearchParams.ToLower()) 
-                || obj.Description.ToLower().Contains(showParams.SearchParams.ToLower()));
+                query = ApplySearchParameters(query, showParams.SearchParams);
 
             return await PagedList<ShowDto>.CreateAsync(
                 query.OrderByDescending(r => r.AverageRating), showParams.PageNumber, showParams.PageSize
